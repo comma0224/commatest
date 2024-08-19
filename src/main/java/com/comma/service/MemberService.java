f1c20ecd8c532ea70c73b7844512fc99f7dea30c
comma <comma0224@gmail.com>
2024-08-20 03:22:18 +0900

    test

---
+++ src/main/java/com/comma/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.comma.service;
+
+import com.comma.domain.Member;
+import com.comma.repository.MemberRepository;
+import com.comma.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+    * 회원가입
+    */
+    public Long join(Member member) {
+
+
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+}
